d517585ab1ee6412e7f7e62b40b11536307102c8
Erick Gonzalez <gempro30yt@gmail.com>
2026-09-22 22:37:31 -0300

    feat: configuracion base de react vite con docker

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "50px",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Panel de Administracion Web</h1>
+      <p>El frontend de ExpertoYa esta corriendo con React, Vite y Docker</p>
+    </div>
+  );
+}
+
+export default App;
