629be59045c26161035e665ae9053837193fcbe5
Erick Gonzalez <gempro30yt@gmail.com>
2026-09-24 23:55:27 -0300

    feat(DAN-9): maquetado de grid profesionales responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Categories } from "./components/categories";
 import { Hero } from "./components/Hero";
+import { ProfessionalsNearYou } from "./components/ProfessionalsNearYou";
 
 function App() {
   return (
     <main className="min-h-screen pb-20">
       <Hero />
       <Categories />
+      <ProfessionalsNearYou />
     </main>
   );
 }
