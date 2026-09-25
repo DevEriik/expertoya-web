c12beca194f0b65a6f9450e393c4a05f52500965
Erick Gonzalez <gempro30yt@gmail.com>
2026-09-25 12:08:41 -0300

    fix: limpiar codigo

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { ProfessionalsNearYou } from "./components/ProfessionalsNearYou";
 import { Navbar } from "./components/Navbar";
 
 function App() {
-  // Agrega esto justo antes del "return" de tu componente:
-  const variableInutil = "Esta variable va a hacer enojar al linter";
-  console.log("Testeando al guardián");
   return (
     <main className="min-h-screen pb-20">
       <Navbar />
