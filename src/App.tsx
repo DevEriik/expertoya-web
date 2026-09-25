c6273affb1f93858d75ea91623a861a7a3f8776a
Erick Gonzalez <gempro30yt@gmail.com>
2026-09-25 11:56:29 -0300

    test: rompiendo el linter a proposito

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { ProfessionalsNearYou } from "./components/ProfessionalsNearYou";
 import { Navbar } from "./components/Navbar";
 
 function App() {
+  // Agrega esto justo antes del "return" de tu componente:
+  const variableInutil = "Esta variable va a hacer enojar al linter";
+  console.log("Testeando al guardián");
   return (
     <main className="min-h-screen pb-20">
       <Navbar />
